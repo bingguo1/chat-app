ad69c7323966f10f9994029dbae95270b00614ae
bingguo1 <changethespace@gmail.com>
2022-01-08 16:23:43 -0500

    first commit of this simple chat app, current work thanks to https://tsh.io/blog/socket-io-tutorial-real-time-communication/ , with functionalities: multiple people talk within same room,  typing recognition

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const socket = require("socket.io");
+
+// App setup
+
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = socket(server);
+
+
+const activeUsers = new Set();
+
+// Static files
+//app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.get('/', (req, res) => {
+    //  res.sendFile(__dirname + '/index.html');
+    //    res.sendFile('jfejfe/index.html');
+    res.sendFile(__dirname+'/public/index.html');
+});
+
+
+io.on("connection", function (socket) {
+    console.log("Made socket connection");
+    
+    socket.on("new user", function (data) {
+    socket.userId = data;
+    activeUsers.add(data);
+    io.emit("new user", [...activeUsers]);
+  });
+
+  socket.on("disconnect", () => {
+    activeUsers.delete(socket.userId);
+    io.emit("user disconnected", socket.userId);
+  });
+
+    socket.on("chat message", function (data) {
+	io.emit("chat message", data);
+    });
+
+    socket.on("typing", function (data) {
+    socket.broadcast.emit("typing", data);
+  });
+    
+});
+
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
